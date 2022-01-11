d8aae705f26ce84218f32601536322c1bfa39e62
bkil <b.K.il.h.u+tigbuh@gmail.com>
2022-01-11 02:55:11 +0100

    src/convert: add bash&sed gemini <-> HTML, TODO: escaping within <pre>

---
+++ src/convert/gemtext2gewtext.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+main() {
+  IN="${1-1.gemtext}"
+
+  TITLE="`grep -o -m1 "[^#].*" "$IN"`"
+
+  cat << EOF
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset=utf-8>
+  <meta http-equiv=content-type content='text/html; charset=UTF-8'>
+  <title>$TITLE</title>
+  <link rel='shortcut icon' type=image/x-icon href=data:image/x-icon;,>
+  <meta name=viewport content='width=device-width, initial-scale=1.0'>
+</head>
+<body>
+EOF
+
+  sed -nr '
+    s~&~\&amp;~g
+    s~<~\&lt;~g
+    s~>~\&gt;~g
+    t loop
+
+    :loop
+    s~^###(.*)~<h3>\1</h3>~
+    t p
+    s~^##(.*)~<h2>\1</h2>~
+    t p
+    s~^#(.*)~<h1>\1</h1>~
+    t p
+    s~^=&gt;([^ ]*)$~<p><a href="\1">\1</a></p>~
+    t p
+    s~^=&gt;([^ ]*) (.*)~<p><a href="\1">\2</a></p>~
+    t p
+
+    s~^```~<pre>~
+    T no_pre
+    N
+    s~\n```$~</pre>~
+    t p
+    s~\n~~
+    t pre
+    :pre
+    N
+    s~\n```$~</pre>~
+    T pre
+    b p
+    
+    :no_pre
+    s~^[*](.*)~<ul>\n<li>\1</li>~
+    T no_list
+    :list
+    p
+    n
+    s~(^)[*](.*)~\1<li>\2</li>~
+    t list
+    h
+    s~.*~</ul>~
+    p
+    g
+    t loop
+
+    :no_list
+    s~.*~<p>&</p>~
+    t p
+    :p
+    p
+    :e
+  ' "$IN"
+
+  cat << EOF
+</body>
+</html>
+EOF
+}
+
+main "$@"
---
+++ src/convert/gewtext2gemtext.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+main() {
+  cat "${1-1.gewtext}" |
+  sed -nr '1,/<(\/head|body)>/ ! p' |
+  sed -r '
+    :l
+    s~<pre>[^<>]*$~&~
+    t e
+    N
+    s~((>)\s*\n\s*|\s*\n\s*(<))~\2\3~
+    t l
+    s~\n~ ~
+    t l
+    :e
+  ' |
+  sed -nr '
+    :loop
+    s~^<h1>([^<]*)</h1>~#\1\n~
+    t p
+    s~^<h2>([^<]*)</h2>~##\1\n~
+    t p
+    s~^<h3>([^<]*)</h3>~###\1\n~
+    t p
+    s~^<li>([^<]*)</li>~*\1\n~
+    t p
+    s~^<a href="([^"]*)">([^<]*)</a>~=>\1\n~
+    t p
+    s~^</ul><ul>~\n~
+    t p
+    s~^<p>([^<]*)</p>~\1\n~
+    t p
+
+    s~^<pre>~```\n~
+    T drop
+    P
+    s~.*\n~~
+    t pre
+
+    :pre
+    s~</pre>~\n~
+    T more_pre
+    P
+    s~.*(\n)~```\1~
+    b p
+
+    :more_pre
+    p
+    n
+    b pre
+
+    :drop
+    s~^<[^>]*>~~
+    t loop
+
+    s~<~\n&~
+    t p
+
+    s~^$~~
+    t e
+
+    :p
+    h
+    s~\n.*~~
+    s~&lt;~<~g
+    s~&gt;~>~g
+    s~&amp;~\&~g
+    p
+    g
+
+    s~.*\n~~
+    t loop
+    :e
+    '
+}
+
+main "$@"
