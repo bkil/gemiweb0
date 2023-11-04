a0771f6ebc1383891e89308fbc549cf3cd1d49a5
bkil <b.K.il.h.u+tigbuh@gmail.com>
2023-11-04 23:22:21 +0100

    src/bf.c/rot1.js: form.text.value.charCodeAt(j)

--- src/bf.c/rot1.js
+++ src/bf.c/rot1.js
@@ -1,1 +1,1 @@
-var i;i=0;var j;j=0;var v;v=new Array;v[i]=form.text.charCodeAt(j)|0;while(v[i]){v[i]=(v[i]|0)+1;document.write(String.fromCharCode(v[i]));j++;v[i]=form.text.charCodeAt(j)|0}
+var i;i=0;var j;j=0;var v;v=new Array;i++;v[i]=form.text.value.charCodeAt(j)|0;while(v[i]){v[i]=(v[i]|0)+1;i++;j++;v[i]=form.text.value.charCodeAt(j)|0};i--;while(v[i]){i--};i++;while(v[i]){document.write(String.fromCharCode(v[i]));i++}
