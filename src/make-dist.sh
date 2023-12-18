8414f465eb14519980d054043f63fae68a69355d
bkil <b.K.il.h.u+tigbuh@gmail.com>
2023-12-18 01:43:45 +0100

    .gitlab-ci,src/make-dist.sh: bundle snapshot zip and compile js0

---
+++ src/make-dist.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+# Copyright (C) 2023 bkil.hu
+# Refer to the GNU GPL v2 in LICENSE for terms
+
+make_head() {
+  cd "$ROOT/src/js3" || return 1
+
+  zip \
+    -j \
+    "$DEST/js0br0.zip" \
+    vm.c \
+    vm-free.c \
+    vm.h \
+    vm-impl.h \
+    include.h \
+    run.c \
+    ../br/br0.js \
+    ../br/lib.js \
+    ../LICENSE \
+    || return 1
+}
+
+make_dev() {
+  cd "$ROOT/src/js3" || return 1
+
+  git ls-files \
+    '*.sh' \
+    'test*' \
+    '*.js' \
+    '../br/*.js' \
+    '../br/*.htm' \
+    |
+  xargs zip \
+    -j \
+    "$DEST/js0br0-dev.zip" \
+    vm-smallbin.c \
+    mk-run \
+    ../LICENSE \
+    || return 1
+}
+
+make_doc() {
+  local S
+  cd "$ROOT" || return 1
+
+  TMP="$DEST/bf-js.md"
+  cp -d --preserve=all "src/bf.c/README.md" "$TMP" || return 1
+
+  git ls-files \
+    'doc/*' \
+    |
+  xargs zip \
+    -j \
+    "$DEST/js0br0-doc.zip" \
+    README.md \
+    LICENSE.md \
+    "$TMP"
+
+  S=$?
+  rm "$TMP"
+  return $S
+}
+
+compile() {
+  local BIN
+  readonly BIN="$DEST/js0"
+
+  cd "$ROOT/src/js3" || return 1
+  gcc \
+    -march=nocona -mtune=broadwell \
+    -Os \
+    -flto \
+    -fno-asynchronous-unwind-tables \
+    -Wl,-z,norelro \
+    -Wl,--build-id=none \
+    -o "$BIN" \
+    run.c vm.c || return 1
+
+  strip \
+    --remove-section=.comment \
+  "$BIN"
+}
+
+main() {
+  local DEST ROOT TMP
+  readonly DEST="`readlink -f "$1"`"
+  readonly ROOT="$(readlink -f "`dirname "$0"`/..")"
+
+  cd "$ROOT" || return 1
+  mkdir -p "$DEST" || return 1
+
+  git ls-files |
+  grep -E '\.(html?|js|css|sh)$' |
+  xargs tar -c |
+  tar -xC "$DEST"
+
+  make_head || return 1
+  make_dev || return 1
+  make_doc || return 1
+  compile || return 1
+}
+
+main "$@"
